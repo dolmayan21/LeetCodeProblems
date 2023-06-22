653a4fc51b966597cab3c8f2f13af51d004fbf91
Dolmayan <alejandrodolmayan@gmail.com>
2023-06-21 22:45:04 -0600

    RemoveDuplicates and MergeSortedArray updated

--- src/main/java/com/java/dolmayan/leetcode/TopInterview150/RemoveDuplicates.java
+++ src/main/java/com/java/dolmayan/leetcode/TopInterview150/RemoveDuplicates.java
@@ -1,4 +1,4 @@
-package com.java.dolmayan.leetcode;
+package com.java.dolmayan.leetcode.TopInterview150;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
