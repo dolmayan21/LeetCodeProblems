49f13185ac255430aea7dc996d1b5aca1592381a
Alex L <alejandrodolmayan@gmail.com>
2023-06-27 19:14:48 -0600

    ContainsDuplicate started

---
+++ src/main/java/com/java/dolmayan/leetcode/EasyColletion/ContainsDuplicate.java
@@ -0,0 +1,31 @@
+package com.java.dolmayan.leetcode.EasyColletion;
+
+public class ContainsDuplicate {
+
+    /*
+    *Given an integer array nums, return true if any
+    * value appears at least twice in the array, and
+    *  return false if every element is distinct.
+    *
+    */
+
+    public static void main(String[] args) {
+
+        int arr[]= {1,2,3,1};
+
+        containsDuplicate(arr);
+
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        boolean flag = true;
+
+        for()
+
+
+
+        return flag;
+    }
+
+}
