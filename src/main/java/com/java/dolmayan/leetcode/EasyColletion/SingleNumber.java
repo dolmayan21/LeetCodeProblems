3b5e175472ae717d2396cda875a86c5e438d7a2b
Dolmayan <alejandrodolmayan@gmail.com>
2023-06-27 21:02:38 -0600

    SingleNumber added (Solved)

---
+++ src/main/java/com/java/dolmayan/leetcode/EasyColletion/SingleNumber.java
@@ -0,0 +1,49 @@
+package com.java.dolmayan.leetcode.EasyColletion;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class SingleNumber {
+
+    public static void main(String[] args) {
+
+       int[] nums = {4,1, 2,1,2};
+
+        //int[] nums = {2,2,1};
+
+        singleNumber(nums);
+
+    }
+    public static int singleNumber(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length; i++){
+            if(stack.isEmpty()){
+                stack.push(nums[i]);
+            }else if(!stack.isEmpty() && stack.peek() == nums[i]){
+                stack.pop();
+            } else if (!stack.isEmpty() && stack.peek() != nums[i]) {
+                stack.push(nums[i]);
+            }
+        }
+        return stack.pop();
+    }
+}
+
+   /*
+   * Holly shit...!
+   *
+    public int singleNumber(int[] nums) {
+        //n XOR 0 = n
+        //n XOR n = 0
+        int ans = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+   * */
