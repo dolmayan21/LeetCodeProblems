97e51759792f1629d886a1a1e40da9f7853f4ff5
Dolmayan <alejandrodolmayan@gmail.com>
2023-06-27 19:45:13 -0600

    ContainsDuplicate added

--- src/main/java/com/java/dolmayan/leetcode/EasyColletion/ContainsDuplicate.java
+++ src/main/java/com/java/dolmayan/leetcode/EasyColletion/ContainsDuplicate.java
@@ -1,31 +1,29 @@
 package com.java.dolmayan.leetcode.EasyColletion;
 
+import java.sql.SQLOutput;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
 
     /*
-    *Given an integer array nums, return true if any
+    * Given an integer array nums, return true if any
     * value appears at least twice in the array, and
     *  return false if every element is distinct.
     *
     */
 
     public static void main(String[] args) {
-
-        int arr[]= {1,2,3,1};
-
+        int arr[]= {0,4,5,0,3,6};
         containsDuplicate(arr);
-
     }
 
     public static boolean containsDuplicate(int[] nums) {
-
-        boolean flag = true;
-
-        for()
-
-
-
-        return flag;
+        Set listSet = new HashSet<>();
+        for(int i=0 ; i<nums.length ; i++){
+            if(!listSet.add(nums[i])) return  true;
+        }
+        return false;
     }
 
 }
