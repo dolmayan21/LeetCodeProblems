cca7eccdcda7d4e93b798ef721bd56c15175db5b
Dolmayan <alejandrodolmayan@gmail.com>
2023-07-06 08:49:09 -0600

    MoveZeroes finished

--- src/main/java/com/java/dolmayan/leetcode/EasyColletion/MoveZeroes.java
+++ src/main/java/com/java/dolmayan/leetcode/EasyColletion/MoveZeroes.java
@@ -1,4 +1,29 @@
 package com.java.dolmayan.leetcode.EasyColletion;
 
+import java.util.Arrays;
+
 public class MoveZeroes {
+
+    public static void main(String[] args) {
+        int arr1[] = {0,1,0,3,12};
+        int arr2[] = {1,0,1};
+
+        moveZeroes(arr1);
+        System.out.println(Arrays.toString(arr1));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int i =0;
+
+           for(int j = 1; j< nums.length; j++){
+                if(nums[i] == 0 && nums[j] != 0){
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    i++;
+                } else if (nums[i] !=0) {
+                    i++;
+                }
+            }
+    }
 }
