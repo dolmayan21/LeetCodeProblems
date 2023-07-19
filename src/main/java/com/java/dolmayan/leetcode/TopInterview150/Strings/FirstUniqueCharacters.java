6c4d9b2c01455214e5d9712fb40237a4263f0425
Dolmayan <alejandrodolmayan@gmail.com>
2023-07-19 13:26:54 -0600

    Not finished

--- src/main/java/com/java/dolmayan/leetcode/TopInterview150/Strings/FirstUniqueCharacters.java
+++ src/main/java/com/java/dolmayan/leetcode/TopInterview150/Strings/FirstUniqueCharacters.java
@@ -1,2 +1,21 @@
-package com.java.dolmayan.leetcode.TopInterview150.Strings;public class FirstUniqueCharacters {
+package com.java.dolmayan.leetcode.TopInterview150.Strings;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class FirstUniqueCharacters {
+    /**
+     * Given a string s, find the first non-repeating character in it
+     * and return its index. If it does not exist, return -1.
+     */
+
+    public static void main(String[] args) {
+
+        String s= "aabb";
+        System.out.println(firstUniqChar(s));
+    }
+    public static int firstUniqChar(String s) {
+        return 1;
+    }
+
 }
