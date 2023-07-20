da15b838941cdc1c00ad5e3ac78d86a53e575ee3
Dolmayan <alejandrodolmayan@gmail.com>
2023-07-20 14:23:20 -0600

    LinkedList added not resolved

---
+++ src/main/java/com/java/dolmayan/leetcode/TopInterview150/Strings/LinkedList.java
@@ -0,0 +1,29 @@
+package com.java.dolmayan.leetcode.TopInterview150.Strings;
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class LinkedList {
+
+    public void deleteNode(ListNode node) {
+
+        int temp = node.val;
+
+        while(node.next != null){
+
+        }
+
+    }
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+}
