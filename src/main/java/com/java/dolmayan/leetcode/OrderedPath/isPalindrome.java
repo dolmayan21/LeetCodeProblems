ddc0841e650082d20dd03d912d94925222905bdd
Dolmayan <alejandrodolmayan@gmail.com>
2023-07-19 19:47:25 -0600

    isPalindrome added

---
+++ src/main/java/com/java/dolmayan/leetcode/OrderedPath/isPalindrome.java
@@ -0,0 +1,21 @@
+package com.java.dolmayan.leetcode.OrderedPath;
+
+public class isPalindrome {
+
+    /**
+     * Given an integer x, return true if x is a palindrome,
+     * and false otherwise.
+     */
+
+    public boolean isPalindrome(int x) {
+
+      String num = String.valueOf(x);
+
+        for(int i=0; i<num.length()/2; i++){
+            if(num.charAt(i) != num.charAt(num.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
