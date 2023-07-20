4d0313ef22344d8406a463dd2bf3ce1eee4d1d77
Dolmayan <alejandrodolmayan@gmail.com>
2023-07-20 14:22:35 -0600

    isValid added not finished

---
+++ src/main/java/com/java/dolmayan/leetcode/OrderedPath/isValid.java
@@ -0,0 +1,13 @@
+package com.java.dolmayan.leetcode.OrderedPath;
+
+public class isValid {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("{[]}"));
+    }
+    public static boolean isValid(String s) {
+
+
+
+    }
+}
