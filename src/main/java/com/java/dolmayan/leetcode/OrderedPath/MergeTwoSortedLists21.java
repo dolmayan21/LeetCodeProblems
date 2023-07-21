6906a05c7ada9f1c31d7aa3f42b0c0e0562030a1
Dolmayan <alejandrodolmayan@gmail.com>
2023-07-20 21:25:32 -0600

    MergeTwoSortedLists21 not solved

---
+++ src/main/java/com/java/dolmayan/leetcode/OrderedPath/MergeTwoSortedLists21.java
@@ -0,0 +1,37 @@
+package com.java.dolmayan.leetcode.OrderedPath;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class MergeTwoSortedLists21 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+
+
+
+        return list1;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+}
