1d05d78390bc194c4c202ca6b8181a3c7085158f
Dolmayan <alejandrodolmayan@gmail.com>
2023-07-21 13:05:40 -0600

    AddBinary67 not finished

---
+++ src/main/java/com/java/dolmayan/leetcode/OrderedPath/AddBinary67.java
@@ -0,0 +1,15 @@
+package com.java.dolmayan.leetcode.OrderedPath;
+
+import java.sql.SQLOutput;
+
+public class AddBinary67 {
+
+    public static void main(String[] args) {
+
+        System.out.println(addBinary("11", "1"));
+    }
+    public static String addBinary(String a, String b) {
+
+        return "";
+    }
+}
